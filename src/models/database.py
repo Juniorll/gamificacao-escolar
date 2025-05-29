06877cd4ea490303a72097271adbb6cab8da72a5
Juniorll <juniorll.hn@gmail.com>
2025-05-28 22:07:00 -0300

    Update database.py

--- src/models/database.py
+++ src/models/database.py
@@ -1,97 +1,116 @@
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 from src.main import db
 
-class Professor(db.Model, UserMixin):
+class Professor(UserMixin, db.Model):
+    __tablename__ = 'professores'
+    
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     senha_hash = db.Column(db.String(200), nullable=False)
-    data_cadastro = db.Column(db.DateTime, default=db.func.current_timestamp())
-    ativo = db.Column(db.Boolean, default=True)
     
-    def get_id(self):
-        return str(self.id)
+    def set_password(self, senha):
+        self.senha_hash = generate_password_hash(senha)
+        
+    def check_password(self, senha):
+        return check_password_hash(self.senha_hash, senha)
 
 class Colegio(db.Model):
+    __tablename__ = 'colegios'
+    
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     endereco = db.Column(db.String(200))
-    logo = db.Column(db.String(200))
-    data_cadastro = db.Column(db.DateTime, default=db.func.current_timestamp())
-    turmas = db.relationship('Turma', backref='colegio', lazy='select')
-
-class ProfessorColegio(db.Model):
-    professor_id = db.Column(db.Integer, db.ForeignKey('professor.id'), primary_key=True)
-    colegio_id = db.Column(db.Integer, db.ForeignKey('colegio.id'), primary_key=True)
+    
+    turmas = db.relationship('Turma', backref='colegio', lazy=True)
 
 class Turma(db.Model):
+    __tablename__ = 'turmas'
+    
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     ano = db.Column(db.Integer, nullable=False)
-    professor_id = db.Column(db.Integer, db.ForeignKey('professor.id'), nullable=False)
-    colegio_id = db.Column(db.Integer, db.ForeignKey('colegio.id'), nullable=False)
-    data_cadastro = db.Column(db.DateTime, default=db.func.current_timestamp())
+    colegio_id = db.Column(db.Integer, db.ForeignKey('colegios.id'), nullable=False)
+    
+    alunos = db.relationship('Aluno', backref='turma', lazy=True)
+    atividades = db.relationship('Atividade', backref='turma', lazy=True)
 
-    alunos = db.relationship('Aluno', backref='turma', lazy='select')
+class Disciplina(db.Model):
+    __tablename__ = 'disciplinas'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    descricao = db.Column(db.Text)
+    icone = db.Column(db.String(50))
+    cor = db.Column(db.String(20))
+    
+    atividades = db.relationship('Atividade', backref='disciplina', lazy=True)
 
 class Periodo(db.Model):
+    __tablename__ = 'periodos'
+    
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     data_inicio = db.Column(db.Date, nullable=False)
     data_fim = db.Column(db.Date, nullable=False)
-    tipo = db.Column(db.String(50), nullable=False)
-    periodo_pai_id = db.Column(db.Integer, db.ForeignKey('periodo.id'))
-
-class Disciplina(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
-    descricao = db.Column(db.Text)
-    professor_id = db.Column(db.Integer, db.ForeignKey('professor.id'), nullable=False)
-    icone = db.Column(db.String(200))
-    cor = db.Column(db.String(7), default='#3498db')
-    data_cadastro = db.Column(db.DateTime, default=db.func.current_timestamp())
+    
+    atividades = db.relationship('Atividade', backref='periodo', lazy=True)
 
 class Aluno(db.Model):
+    __tablename__ = 'alunos'
+    
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     foto = db.Column(db.String(200))
-    avatar = db.Column(db.String(200))
-    turma_id = db.Column(db.Integer, db.ForeignKey('turma.id'), nullable=False)
-    data_cadastro = db.Column(db.DateTime, default=db.func.current_timestamp())
+    turma_id = db.Column(db.Integer, db.ForeignKey('turmas.id'), nullable=False)
+    nivel = db.Column(db.Integer, default=1)
+    xp = db.Column(db.Integer, default=0)
+    categoria = db.Column(db.String(50), default='Bronze')
+    
+    notas = db.relationship('Nota', backref='aluno', lazy=True)
+    trabalhos = db.relationship('Trabalho', backref='aluno', lazy=True)
 
 class Atividade(db.Model):
+    __tablename__ = 'atividades'
+    
     id = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String(100), nullable=False)
     descricao = db.Column(db.Text)
-    tipo = db.Column(db.String(50), nullable=False)
-    valor_max = db.Column(db.Float, nullable=False)
-    peso = db.Column(db.Float, default=1.0)
+    data_criacao = db.Column(db.DateTime, default=datetime.utcnow)
     data_entrega = db.Column(db.Date, nullable=False)
-    professor_id = db.Column(db.Integer, db.ForeignKey('professor.id'), nullable=False)
-    disciplina_id = db.Column(db.Integer, db.ForeignKey('disciplina.id'), nullable=False)
-    periodo_id = db.Column(db.Integer, db.ForeignKey('periodo.id'), nullable=False)
-    data_cadastro = db.Column(db.DateTime, default=db.func.current_timestamp())
+    peso = db.Column(db.Float, default=1.0)
+    xp_base = db.Column(db.Integer, default=100)
+    turma_id = db.Column(db.Integer, db.ForeignKey('turmas.id'), nullable=False)
+    disciplina_id = db.Column(db.Integer, db.ForeignKey('disciplinas.id'), nullable=False)
+    periodo_id = db.Column(db.Integer, db.ForeignKey('periodos.id'), nullable=False)
+    
+    notas = db.relationship('Nota', backref='atividade', lazy=True)
 
 class Nota(db.Model):
+    __tablename__ = 'notas'
+    
     id = db.Column(db.Integer, primary_key=True)
-    aluno_id = db.Column(db.Integer, db.ForeignKey('aluno.id'), nullable=False)
-    atividade_id = db.Column(db.Integer, db.ForeignKey('atividade.id'), nullable=False)
     valor = db.Column(db.Float, nullable=False)
-    realizada = db.Column(db.Boolean, default=True)
-    data_lancamento = db.Column(db.DateTime, default=db.func.current_timestamp())
+    xp_ganho = db.Column(db.Integer, nullable=False)
+    data_lancamento = db.Column(db.DateTime, default=datetime.utcnow)
+    trabalho_exemplar = db.Column(db.Boolean, default=False)
+    aluno_id = db.Column(db.Integer, db.ForeignKey('alunos.id'), nullable=False)
+    atividade_id = db.Column(db.Integer, db.ForeignKey('atividades.id'), nullable=False)
 
-class TrabalhoExemplar(db.Model):
+class Trabalho(db.Model):
+    __tablename__ = 'trabalhos'
+    
     id = db.Column(db.Integer, primary_key=True)
-    aluno_id = db.Column(db.Integer, db.ForeignKey('aluno.id'), nullable=False)
-    atividade_id = db.Column(db.Integer, db.ForeignKey('atividade.id'), nullable=False)
+    titulo = db.Column(db.String(100), nullable=False)
     descricao = db.Column(db.Text)
-    link = db.Column(db.String(255))
-    arquivo = db.Column(db.String(255))
-    data_destaque = db.Column(db.DateTime, default=db.func.current_timestamp())
-
-class Configuracao(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    chave = db.Column(db.String(100), nullable=False, unique=True)
-    valor = db.Column(db.Text, nullable=False)
-    descricao = db.Column(db.Text)
+    imagem = db.Column(db.String(200))
+    data = db.Column(db.DateTime, default=datetime.utcnow)
+    nota = db.Column(db.Float, nullable=False)
+    comentario = db.Column(db.Text)
+    aluno_id = db.Column(db.Integer, db.ForeignKey('alunos.id'), nullable=False)
+    disciplina_id = db.Column(db.Integer, db.ForeignKey('disciplinas.id'), nullable=False)
+    turma_id = db.Column(db.Integer, db.ForeignKey('turmas.id'), nullable=False)
+    periodo_id = db.Column(db.Integer, db.ForeignKey('periodos.id'), nullable=False)
